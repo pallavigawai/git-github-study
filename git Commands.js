9f55fe79e130d3f1dc960774c52a7f6d3208917f
pallavi <99071482+pallavigawai@users.noreply.github.com>
2022-11-02 22:18:26 +0530

    Create git Commands.js

---
+++ git Commands.js
@@ -0,0 +1,11 @@
+/**************************** A List of Git Commands *****************************/
+1. git init [directory]                 :Create a new Git repository from an existing directory
+2. git clone [repo / URL]               :Clone a repository (local or remote via HTTP/SSH)
+3. git add [filename]                   :Git notifies you if you have any untracked files. If you want Git to start tracking a file, run the following command
+4. git reset [commit] -- [file_path]    :Unstage Files on Git
+   Working with Git usually involves adding all the files to your index to prepare them for a commit. If you want to remove some files from the index before committing, you have to unstage the files in Git.
+   One way to unstage files on Git is to run the git reset command.
+5. git commit -m "Notes about the commit": Create a Commit
+   After adding the specified files to the staging environment, instruct Git to package the files into a commit using the git commit command. Git then stores that file version. You can review the stored version at any given time.   
+
+   
