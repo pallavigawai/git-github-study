c31f916c8a5d05db71ab38b1c9f9bea5ee023f3e
Pallavi Gawai <pallavigawai2022@gmail.com>
2023-03-21 17:01:10 +0530

    Update git Commands.js

--- git Commands.js
+++ git Commands.js
@@ -1,6 +1,6 @@
 /**************************** A List of Git Commands *****************************/
 1. git init [directory]                 :Create a new Git repository from an existing directory
-2. git clone [repo / URL]               :Clone a repository (local or remote via HTTP/SSH)
+2. git clone [repo / URL]               :Clone a repository (local or remote via HTTP/SSH) used to create copy of a specific repository or branch within a repository.
 3. git add [filename]                   :Git notifies you if you have any untracked files. If you want Git to start tracking a file, run the following command
 4. git reset [commit] -- [file_path]    :Unstage Files on Git
    Working with Git usually involves adding all the files to your index to prepare them for a commit. If you want to remove some files from the index before committing, you have to unstage the files in Git.
